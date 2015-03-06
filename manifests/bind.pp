5caa36c10d9d9809d0c1978096ffe838439e9ea4
Alex Unigovsky <unik@compot.ru>
2015-03-06 07:50:17 +0300

    More work on BIND

--- manifests/bind.pp
+++ manifests/bind.pp
@@ -5,14 +5,41 @@ class npconfgen::bind (
 	$confgen_service  = 'iscbind99',
 	$package_name     = $::npconfgen::params::bind_package_name,
 	$package_provider = $::npconfgen::params::package_provider,
-	$user             = $::npconfgen::params::bind_user,
-	$group            = $::npconfgen::params::bind_group,
+	$track_conf_dir   = $::npconfgen::params::bind_track_conf_dir,
 	$conf_dir         = $::npconfgen::params::bind_conf_dir,
+	$conf_dir_user    = $::npconfgen::params::bind_conf_dir_user,
+	$conf_dir_group   = $::npconfgen::params::bind_conf_dir_group,
+	$conf_dir_mode    = $::npconfgen::params::bind_conf_dir_mode,
 	$zones_dir        = $::npconfgen::params::bind_zones_dir,
+	$zones_dir_user   = $::npconfgen::params::bind_zones_dir_user,
+	$zones_dir_group  = $::npconfgen::params::bind_zones_dir_group,
+	$zones_dir_mode   = $::npconfgen::params::bind_zones_dir_mode,
+	$work_dir         = $::npconfgen::params::bind_work_dir,
+	$work_dir_user    = $::npconfgen::params::bind_work_dir_user,
+	$work_dir_group   = $::npconfgen::params::bind_work_dir_group,
+	$work_dir_mode    = $::npconfgen::params::bind_work_dir_mode,
 	$keys_dir         = $::npconfgen::params::bind_keys_dir,
+	$keys_dir_user    = $::npconfgen::params::bind_keys_dir_user,
+	$keys_dir_group   = $::npconfgen::params::bind_keys_dir_group,
+	$keys_dir_mode    = $::npconfgen::params::bind_keys_dir_mode,
 	$pri_dir          = $::npconfgen::params::bind_pri_dir,
 	$sec_dir          = $::npconfgen::params::bind_sec_dir,
 	$rev_dir          = $::npconfgen::params::bind_rev_dir,
+	$rndc_file        = $::npconfgen::params::bind_rndc_file,
+	$rndc_key         = $::npconfgen::params::bind_rndc_key,
+	$pid_file         = $::npconfgen::params::bind_pid_file,
+	$sesskey_file     = $::npconfgen::params::bind_sesskey_file,
+	$stats_file       = $::npconfgen::params::bind_stats_file,
+	$memstats_file    = $::npconfgen::params::bind_memstats_file,
+	$dump_file        = $::npconfgen::params::bind_dump_file,
+	$keys_file        = $::npconfgen::params::bind_keys_file,
+	$managedkeys_dir  = $::npconfgen::params::bind_managedkeys_dir,
+	$zone_root        = $::npconfgen::params::bind_zone_root,
+	$zone_empty       = $::npconfgen::params::bind_zone_empty,
+	$zone_localhost   = $::npconfgen::params::bind_zone_localhost,
+	$zone_127         = $::npconfgen::params::bind_zone_127,
+	$zone_0           = $::npconfgen::params::bind_zone_0,
+	$zone_255         = $::npconfgen::params::bind_zone_255,
 	$service_name     = $::npconfgen::params::bind_service_name,
 	$service_ensure   = running,
 ) inherits ::npconfgen::params {
@@ -20,6 +47,41 @@ class npconfgen::bind (
 	package { $package_name:
 		ensure   => installed,
 		provider => $package_provider,
+	}
+
+	if $track_conf_dir {
+		file { $conf_dir:
+			ensure  => directory,
+			owner   => $conf_dir_user,
+			group   => $conf_dir_group,
+			mode    => $conf_dir_mode,
+			require => Package[$package_name],
+		}
+	}
+	if ($zones_dir != $conf_dir) or (not $track_conf_dir) {
+		file { $zones_dir:
+			ensure  => directory,
+			owner   => $zones_dir_user,
+			group   => $zones_dir_group,
+			mode    => $zones_dir_mode,
+			require => Package[$package_name],
+		}
+	}
+	if (($work_dir != $conf_dir) and $track_conf_dir) or ($work_dir != $zones_dir) {
+		file { $work_dir:
+			ensure  => directory,
+			owner   => $work_dir_user,
+			group   => $work_dir_group,
+			mode    => $work_dir_mode,
+			require => Package[$package_name],
+		}
+	}
+	file { $keys_dir:
+		ensure  => directory,
+		owner   => $keys_dir_user,
+		group   => $keys_dir_group,
+		mode    => $keys_dir_mode,
+		require => Package[$package_name],
 	}
 }
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -12,94 +12,205 @@ class npconfgen::params {
 			} else {
 				$dhcp_dir = '/etc/dhcp'
 			}
-			$dhcp_package_name = 'isc-dhcp-server'
-			$dhcp_service_name = 'isc-dhcp-server'
+			$dhcp_package_name    = 'isc-dhcp-server'
+			$dhcp_service_name    = 'isc-dhcp-server'
 
-			$bind_conf_dir     = '/etc/bind'
-			$bind_zones_dir    = '/etc/bind'
-			$bind_keys_dir     = '/etc/bind/keys'
-			$bind_pri_dir      = 'pri'
-			$bind_sec_dir      = 'sec'
-			$bind_rev_dir      = 'rev'
-			$bind_package_name = 'bind9'
-			$bind_service_name = 'bind9'
-			$bind_user         = 'bind'
-			$bind_group        = 'bind'
+			$bind_track_conf_dir  = true
+			$bind_conf_dir        = '/etc/bind'
+			$bind_conf_dir_user   = 'root'
+			$bind_conf_dir_group  = 'bind'
+			$bind_conf_dir_mode   = '2755'
+			$bind_zones_dir       = '/etc/bind'
+			$bind_zones_dir_user  = 'root'
+			$bind_zones_dir_group = 'bind'
+			$bind_zones_dir_mode  = '2755'
+			$bind_work_dir        = '/var/cache/bind'
+			$bind_work_dir_user   = 'root'
+			$bind_work_dir_group  = 'bind'
+			$bind_work_dir_mode   = '0775'
+			$bind_keys_dir        = '/etc/bind/keys'
+			$bind_keys_dir_user   = 'root'
+			$bind_keys_dir_group  = 'bind'
+			$bind_keys_dir_mode   = '0750'
+			$bind_pri_dir         = 'pri'
+			$bind_sec_dir         = 'sec'
+			$bind_rev_dir         = 'rev'
+			$bind_rndc_file       = '/etc/bind/rndc.key'
+			$bind_rndc_key        = 'rndc-key'
+			$bind_pid_file        = undef
+			$bind_sesskey_file    = undef
+			$bind_stats_file      = 'named.stats'
+			$bind_memstats_file   = 'named.memstats'
+			$bind_dump_file       = 'named_dump.db'
+			$bind_keys_file       = undef
+			$bind_managedkeys_dir = undef
+			$bind_zone_root       = '/etc/bind/db.root'
+			$bind_zone_empty      = '/etc/bind/db.empty'
+			$bind_zone_localhost  = '/etc/bind/db.local'
+			$bind_zone_127        = '/etc/bind/db.127'
+			$bind_zone_0          = '/etc/bind/db.0'
+			$bind_zone_255        = '/etc/bind/db.255'
+			$bind_package_name    = 'bind9'
+			$bind_service_name    = 'bind9'
 
-			$package_provider  = undef
-		}
-		'darwin': {
-			$dhcp_dir          = '/opt/local/etc/dhcp'
-			$dhcp_package_name = 'dhcp'
-			$dhcp_service_name = 'org.macports.dhcpd'
-
-			$bind_conf_dir     = '/opt/local/etc'
-			$bind_zones_dir    = '/opt/local/var/named'
-			$bind_keys_dir     = '/opt/local/etc/named/keys'
-			$bind_pri_dir      = 'pri'
-			$bind_sec_dir      = 'sec'
-			$bind_rev_dir      = 'rev'
-			$bind_package_name = 'bind9'
-			$bind_service_name = 'org.macports.bind9'
-			$bind_user         = 'root'
-			$bind_group        = 'named'
-
-			$package_provider  = 'macports'
-		}
-		'freebsd': {
-			$dhcp_dir          = '/usr/local/etc'
-			$dhcp_package_name = 'net/isc-dhcp42-server'
-			$dhcp_service_name = 'isc-dhcpd'
-
-			$bind_conf_dir     = '/etc/namedb'
-			$bind_zones_dir    = '/etc/namedb'
-			$bind_keys_dir     = '/etc/namedb/keys'
-			$bind_pri_dir      = 'pri'
-			$bind_sec_dir      = 'sec'
-			$bind_rev_dir      = 'rev'
-			$bind_package_name = 'dns/bind910'
-			$bind_service_name = 'named'
-			$bind_user         = 'bind'
-			$bind_group        = 'bind'
-
-			$package_provider  = undef
+			$package_provider     = undef
 		}
 		'redhat': {
-			$dhcp_dir          = '/etc/dhcp'
-			$dhcp_package_name = 'dhcp'
-			$dhcp_service_name = 'dhcpd'
+			$dhcp_dir             = '/etc/dhcp'
+			$dhcp_package_name    = 'dhcp'
+			$dhcp_service_name    = 'dhcpd'
 
-			$bind_conf_dir     = '/etc'
-			$bind_zones_dir    = '/etc/named'
-			$bind_keys_dir     = '/etc/named/keys'
-			$bind_pri_dir      = 'pri'
-			$bind_sec_dir      = 'sec'
-			$bind_rev_dir      = 'rev'
-			$bind_package_name = 'bind'
-			$bind_service_name = 'named'
-			$bind_user         = 'root'
-			$bind_group        = 'named'
+			$bind_track_conf_dir  = false
+			$bind_conf_dir        = '/etc'
+			$bind_conf_dir_user   = 'root'
+			$bind_conf_dir_group  = 'root'
+			$bind_conf_dir_mode   = '0755'
+			$bind_zones_dir       = '/etc/named'
+			$bind_zones_dir_user  = 'root'
+			$bind_zones_dir_group = 'named'
+			$bind_zones_dir_mode  = '0750'
+			$bind_work_dir        = '/var/named'
+			$bind_work_dir_user   = 'root'
+			$bind_work_dir_group  = 'named'
+			$bind_work_dir_mode   = '0750'
+			$bind_keys_dir        = '/etc/named/keys'
+			$bind_keys_dir_user   = 'root'
+			$bind_keys_dir_group  = 'named'
+			$bind_keys_dir_mode   = '0750'
+			$bind_pri_dir         = 'pri'
+			$bind_sec_dir         = 'sec'
+			$bind_rev_dir         = 'rev'
+			$bind_rndc_file       = undef
+			$bind_rndc_key        = undef
+			$bind_pid_file        = '/run/named/named.pid'
+			$bind_sesskey_file    = '/run/named/session.key'
+			$bind_stats_file      = '/var/named/data/named_stats.txt'
+			$bind_memstats_file   = '/var/named/data/named_mem_stats.txt'
+			$bind_dump_file       = '/var/named/data/cache_dump.db'
+			$bind_keys_file       = '/etc/named.iscdlv.key'
+			$bind_managedkeys_dir = '/var/named/dynamic'
+			$bind_zone_root       = 'named.ca'
+			$bind_zone_empty      = 'named.empty'
+			$bind_zone_localhost  = 'named.localhost'
+			$bind_zone_127        = undef # was 'named.loopback'
+			$bind_zone_0          = 'named.empty'
+			$bind_zone_255        = 'named.empty'
+			$bind_package_name    = 'bind'
+			$bind_service_name    = 'named'
 
-			$package_provider  = undef
+			$package_provider     = undef
 		}
 		'gentoo': {
-			$dhcp_dir          = '/etc/dhcp'
-			$dhcp_package_name = 'net-misc/dhcp'
-			$dhcp_service_name = 'dhcpd'
+			$dhcp_dir             = '/etc/dhcp'
+			$dhcp_package_name    = 'net-misc/dhcp'
+			$dhcp_service_name    = 'dhcpd'
 
-			$bind_conf_dir     = '/etc/bind'
-			$bind_zones_dir    = '/var/bind'
-			$bind_keys_dir     = '/etc/bind/keys'
-			$bind_pri_dir      = 'pri'
-			$bind_sec_dir      = 'sec'
-			$bind_rev_dir      = 'rev'
-			$bind_package_name = 'net-dns/bind'
-			$bind_service_name = 'named'
-			$bind_user         = 'root'
-			$bind_group        = 'named'
+			$bind_track_conf_dir  = true
+			$bind_conf_dir        = '/etc/bind'
+			$bind_conf_dir_user   = 'root'
+			$bind_conf_dir_group  = 'named'
+			$bind_conf_dir_mode   = '0750'
+			$bind_zones_dir       = '/var/bind'
+			$bind_zones_dir_user  = 'root'
+			$bind_zones_dir_group = 'named'
+			$bind_zones_dir_mode  = '0770'
+			$bind_work_dir        = '/var/bind'
+			$bind_work_dir_user   = 'root'
+			$bind_work_dir_group  = 'named'
+			$bind_work_dir_mode   = '0770'
+			$bind_keys_dir        = '/etc/bind/keys'
+			$bind_keys_dir_user   = 'root'
+			$bind_keys_dir_group  = 'named'
+			$bind_keys_dir_mode   = '0750'
+			$bind_pri_dir         = 'pri'
+			$bind_sec_dir         = 'sec'
+			$bind_rev_dir         = 'rev'
+			$bind_rndc_file       = '/etc/bind/rndc.key'
+			$bind_rndc_key        = 'rndc-key'
+			$bind_pid_file        = '/run/named/named.pid'
+			$bind_sesskey_file    = '/run/named/session.key'
+			$bind_stats_file      = 'named.stats'
+			$bind_memstats_file   = 'named.memstats'
+			$bind_dump_file       = 'named_dump.db'
+			$bind_keys_file       = '/etc/bind/bind.keys'
+			$bind_managedkeys_dir = undef
+			$bind_zone_root       = '/var/bind/named.cache'
+			$bind_zone_empty      = undef
+			$bind_zone_localhost  = 'pri/localhost.zone'
+			$bind_zone_127        = undef
+			$bind_zone_0          = undef
+			$bind_zone_255        = undef
+			$bind_package_name    = 'net-dns/bind'
+			$bind_service_name    = 'named'
 
-			$package_provider  = undef
+			$package_provider     = undef
 		}
+		'freebsd': {
+			$dhcp_dir             = '/usr/local/etc'
+			$dhcp_package_name    = 'net/isc-dhcp43-server'
+			$dhcp_service_name    = 'isc-dhcpd'
+
+			$bind_track_conf_dir  = true
+			$bind_conf_dir        = '/usr/local/etc/namedb'
+			$bind_conf_dir_user   = 'root'
+			$bind_conf_dir_group  = 'wheel'
+			$bind_conf_dir_mode   = '0755'
+			$bind_zones_dir       = '/usr/local/etc/namedb'
+			$bind_zones_dir_user  = 'root'
+			$bind_zones_dir_group = 'wheel'
+			$bind_zones_dir_mode  = '0755'
+			$bind_work_dir        = '/usr/local/etc/namedb/working'
+			$bind_work_dir_user   = 'bind'
+			$bind_work_dir_group  = 'bind'
+			$bind_work_dir_mode   = '0755'
+			$bind_keys_dir        = '/usr/local/etc/namedb/keys'
+			$bind_keys_dir_user   = 'root'
+			$bind_keys_dir_group  = 'bind'
+			$bind_keys_dir_mode   = '0750'
+			$bind_pri_dir         = 'master'
+			$bind_sec_dir         = 'slave'
+			$bind_rev_dir         = 'revzones'
+			$bind_rndc_file       = '/usr/local/etc/namedb/rndc.key'
+			$bind_rndc_key        = 'rndc-key'
+			$bind_pid_file        = '/var/run/named/pid'
+			$bind_sesskey_file    = undef
+			$bind_stats_file      = '/var/stats/named.stats'
+			$bind_memstats_file   = '/var/stats/named.memstats'
+			$bind_dump_file       = '/var/dump/named_dump.db'
+			$bind_keys_file       = undef
+			$bind_managedkeys_dir = undef
+			$bind_zone_root       = '/usr/local/etc/namedb/named.root'
+			$bind_zone_empty      = '/usr/local/etc/namedb/master/empty.db'
+			$bind_zone_localhost  = '/usr/local/etc/namedb/master/localhost-forward.db'
+			$bind_zone_127        = '/usr/local/etc/namedb/master/localhost-reverse.db'
+			$bind_zone_0          = '/usr/local/etc/namedb/master/empty.db'
+			$bind_zone_255        = '/usr/local/etc/namedb/master/empty.db'
+			$bind_package_name    = 'dns/bind910'
+			$bind_service_name    = 'named'
+
+			$package_provider     = undef
+		}
+		#'darwin': {
+		#	$dhcp_dir             = '/opt/local/etc/dhcp'
+		#	$dhcp_package_name    = 'dhcp'
+		#	$dhcp_service_name    = 'org.macports.dhcpd'
+		#
+		#	$bind_track_conf_dir  = false
+		#	$bind_conf_dir        = '/opt/local/etc'
+		#	$bind_zones_dir       = '/opt/local/var/named'
+		#	$bind_work_dir        = '/opt/local/var/named'
+		#	$bind_keys_dir        = '/opt/local/etc/named/keys'
+		#	$bind_keys_dir_user   = 'root'
+		#	$bind_keys_dir_group  = 'named'
+		#	$bind_keys_dir_mode   = '0750'
+		#	$bind_pri_dir         = 'pri'
+		#	$bind_sec_dir         = 'sec'
+		#	$bind_rev_dir         = 'rev'
+		#	$bind_package_name    = 'bind9'
+		#	$bind_service_name    = 'org.macports.bind9'
+		#
+		#	$package_provider     = 'macports'
+		#}
 		default: {
 			fail('NetProfile configuration deployment via Puppet is currently unsupported on ${::osfamily}')
 		}
